ef5ef66960f7b28a688c6cf314bbc57233094788
bassfault <herbert71@hotmail.it>
2021-10-01 17:31:45 +0200

    testing io

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
+const fs = std.fs; 
 
 pub fn main() anyerror!void {
+    var f = try fs.openFileAbsolute("C:/temp/03111588.txt", fs.File.OpenFlags{.read = true}); 
+    defer f.close(); 
+
+    var buf: [1024]u8 = undefined; 
+    var bytes_read = try f.read(buf[0..]);
+
+    const stdout = std.io.getStdOut().writer();
+
+    while (bytes_read > 0)
+    {
+        try stdout.print("{s}", .{buf[0..bytes_read]});
+        bytes_read = try f.read(buf[0..]);
+    }
+    
     std.log.info("All your codebase are belong to us.", .{});
 }
