f41845533f8d6ba500f7851e876fc3c7465582a9
bassfault <herbert71@hotmail.it>
2021-10-04 14:41:54 +0200

    Setting up allocator.

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,24 @@
 const std = @import("std");
-const fs = std.fs; 
+const fs = std.fs;
 
 pub fn main() anyerror!void {
-    var f = try fs.openFileAbsolute("C:/temp/03111588.txt", fs.File.OpenFlags{.read = true}); 
-    defer f.close(); 
+    // Setup the main allocator (useful for debugging)
+    var gpalloc = std.heap.GeneralPurposeAllocator(.{}){};
+    var alloc = &gpalloc.allocator;
+    defer _ = gpalloc.deinit();
 
-    var buf: [1024]u8 = undefined; 
+    var f = try fs.openFileAbsolute("C:/temp/03111588.txt", .{ .read = true });
+    defer f.close();
+
+    var buf: [1024]u8 = undefined;
     var bytes_read = try f.read(buf[0..]);
 
     const stdout = std.io.getStdOut().writer();
 
-    while (bytes_read > 0)
-    {
+    while (bytes_read > 0) {
         try stdout.print("{s}", .{buf[0..bytes_read]});
         bytes_read = try f.read(buf[0..]);
     }
-    
+
     std.log.info("All your codebase are belong to us.", .{});
 }
