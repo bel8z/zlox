fb6250dbd170e6c497118052f5baabef713ac82c
bassfault <herbert71@hotmail.it>
2021-10-08 09:57:45 +0200

    Made scanner produce tokens lazily instead of filling a list

--- src/lox.zig
+++ src/lox.zig
@@ -40,13 +40,16 @@ pub fn runFile(allocator: *mem.Allocator, filename: []u8) !void {
 }
 
 fn run(allocator: *mem.Allocator, bytes: []u8) !void {
-    var scanner = Scanner.init(allocator, bytes);
-    defer scanner.deinit();
-
-    var tokens = try scanner.scanTokens();
     var stdout = io.getStdOut().writer();
 
-    for (tokens.items) |token| {
+    var scanner = Scanner.init(bytes);
+
+    // TODO (Matteo): Is a list really needed? Maybe for later parsing
+    var tokens = List(Token).init(allocator);
+    defer tokens.deinit();
+
+    while (scanner.scanToken()) |token| {
+        try tokens.append(token);
         try stdout.print("{}\n", .{token});
     }
 }
@@ -152,87 +155,86 @@ const Scanner = struct {
     const Self = @This();
 
     source: []u8,
-
-    tokens: List(Token),
-
     start: usize = 0,
     current: usize = 0,
     line: usize = 1,
 
-    fn init(allocator: *mem.Allocator, bytes: []u8) Self {
+    fn init(bytes: []u8) Self {
         return Self{
             .source = bytes,
-            .tokens = List(Token).init(allocator),
         };
     }
 
-    fn deinit(self: *Self) void {
-        self.tokens.deinit();
-    }
+    fn scanToken(self: *Self) ?Token {
+        if (self.isAtEnd()) return null;
 
-    fn scanTokens(self: *Self) !List(Token) {
         while (!self.isAtEnd()) {
             // We are at the beginning of the next lexeme.
             self.start = self.current;
-            try self.scanToken();
+
+            var c = self.advance();
+
+            switch (c) {
+                // Punctuation
+                '(' => return self.addToken(TokenType.LEFT_PAREN),
+                ')' => return self.addToken(TokenType.RIGHT_PAREN),
+                '{' => return self.addToken(TokenType.LEFT_BRACE),
+                '}' => return self.addToken(TokenType.RIGHT_BRACE),
+                ',' => return self.addToken(TokenType.COMMA),
+                '.' => return self.addToken(TokenType.DOT),
+                '-' => return self.addToken(TokenType.MINUS),
+                '+' => return self.addToken(TokenType.PLUS),
+                ';' => return self.addToken(TokenType.SEMICOLON),
+                '*' => return self.addToken(TokenType.STAR),
+                // *_EQUAL operators
+                '!' => return self.addToken(if (self.match('=')) TokenType.BANG_EQUAL else TokenType.BANG),
+                '=' => return self.addToken(if (self.match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL),
+                '<' => return self.addToken(if (self.match('=')) TokenType.LESS_EQUAL else TokenType.LESS),
+                '>' => return self.addToken(if (self.match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER),
+                '/' => {
+                    if (self.match('/')) {
+                        while (self.peek() != '\n' and !self.isAtEnd()) {
+                            // Discard comment characters until the line end
+                            _ = self.advance();
+                        }
+                    } else {
+                        return self.addToken(TokenType.SLASH);
+                    }
+                },
+                // Whitespace
+                ' ', '\r', 't' => {},
+                '\n' => self.line += 1,
+                // Identifiers
+                // Strings
+                '"' => if (self.string()) |s| {
+                    return s;
+                } else |err| switch (err) {
+                    error.UnterminatedString => {
+                        // TODO (Matteo): Better error handling
+                        reportError(self.line, "Unterminated string.", .{}) catch unreachable;
+                    },
+                    else => unreachable,
+                },
+                //
+                else => {
+                    if (isDigit(c)) {
+                        return self.number();
+                    } else if (isAlpha(c)) {
+                        return self.identifier();
+                    } else {
+                        // TODO (Matteo): better error handling
+                        reportError(self.line, "Invalid token: {}", .{c}) catch unreachable;
+                    }
+                },
+            }
         }
 
-        try self.tokens.append(Token{
+        return Token{
             .type = .EOF,
             .literal = Literal.none,
             .lexeme = "",
             .line = self.line,
-        });
-
-        return self.tokens;
-    }
-
-    fn scanToken(self: *Self) !void {
-        var c = self.advance();
-        switch (c) {
-            // Punctuation
-            '(' => try self.addToken(TokenType.LEFT_PAREN),
-            ')' => try self.addToken(TokenType.RIGHT_PAREN),
-            '{' => try self.addToken(TokenType.LEFT_BRACE),
-            '}' => try self.addToken(TokenType.RIGHT_BRACE),
-            ',' => try self.addToken(TokenType.COMMA),
-            '.' => try self.addToken(TokenType.DOT),
-            '-' => try self.addToken(TokenType.MINUS),
-            '+' => try self.addToken(TokenType.PLUS),
-            ';' => try self.addToken(TokenType.SEMICOLON),
-            '*' => try self.addToken(TokenType.STAR),
-            // *_EQUAL operators
-            '!' => try self.addToken(if (self.match('=')) TokenType.BANG_EQUAL else TokenType.BANG),
-            '=' => try self.addToken(if (self.match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL),
-            '<' => try self.addToken(if (self.match('=')) TokenType.LESS_EQUAL else TokenType.LESS),
-            '>' => try self.addToken(if (self.match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER),
-            '/' => {
-                if (self.match('/')) {
-                    while (self.peek() != '\n' and !self.isAtEnd()) {
-                        // Discard comment characters until the line end
-                        _ = self.advance();
-                    }
-                } else {
-                    try self.addToken(TokenType.SLASH);
-                }
-            },
-            // Whitespace
-            ' ', '\r', 't' => {},
-            '\n' => self.line += 1,
-            // Identifiers
-            // Strings
-            '"' => try self.string(),
-            //
-            else => {
-                if (isDigit(c)) {
-                    try self.number();
-                } else if (isAlpha(c)) {
-                    try self.identifier();
-                } else {
-                    try reportError(self.line, "Invalid token: {}", .{c});
-                }
-            },
-        }
+        };
     }
 
     fn isAtEnd(self: *const Self) bool {
@@ -261,7 +263,7 @@ const Scanner = struct {
         return if (next >= self.source.len) 0 else self.source[next];
     }
 
-    fn string(self: *Self) !void {
+    fn string(self: *Self) !Token {
         while (self.peek() != '"' and !self.isAtEnd()) {
             if (self.peek() == '\n') {
                 self.line += 1;
@@ -270,8 +272,7 @@ const Scanner = struct {
         }
 
         if (self.isAtEnd()) {
-            try reportError(self.line, "Unterminated string.", .{});
-            return;
+            return error.UnterminatedString;
         }
 
         // The closing ".
@@ -279,10 +280,10 @@ const Scanner = struct {
 
         // Trim the surrounding quotes.
         var value = self.source[self.start + 1 .. self.current - 1];
-        try self.addTokenLiteral(TokenType.STRING, Literal{ .string = value });
+        return self.addTokenLiteral(TokenType.STRING, Literal{ .string = value });
     }
 
-    fn number(self: *Self) !void {
+    fn number(self: *Self) Token {
         while (isDigit(self.peek())) {
             _ = self.advance();
         }
@@ -300,42 +301,44 @@ const Scanner = struct {
         var lexeme = self.source[self.start..self.current];
         var value = std.fmt.parseFloat(f64, lexeme) catch unreachable;
 
-        try self.tokens.append(Token{
+        return Token{
             .type = TokenType.NUMBER,
             .literal = Literal{ .number = value },
             .lexeme = lexeme,
             .line = self.line,
-        });
+        };
     }
 
-    fn identifier(self: *Self) !void {
+    fn identifier(self: *Self) Token {
         while (isAlphaNumeric(self.peek())) {
             _ = self.advance();
         }
 
         var value = self.source[self.start..self.current];
 
-        try self.tokens.append(Token{
+        return Token{
             .type = TokenType.IDENTIFIER,
             .literal = Literal{ .identifier = value },
             .lexeme = value,
             .line = self.line,
-        });
+        };
     }
 
-    fn addToken(self: *Self, tok_type: TokenType) !void {
-        try self.addTokenLiteral(tok_type, Literal.none);
+    fn addToken(self: *Self, tok_type: TokenType) Token {
+        return self.addTokenLiteral(tok_type, Literal.none);
     }
 
-    fn addTokenLiteral(self: *Self, tok_type: TokenType, literal: Literal) !void {
-        try self.tokens.append(Token{
+    fn addTokenLiteral(self: *Self, tok_type: TokenType, literal: Literal) Token {
+        return Token{
             .type = tok_type,
             .literal = literal,
             .lexeme = self.source[self.start..self.current],
             .line = self.line,
-        });
+        };
     }
 };
+
+//=== Utilities ===//
 
 fn isDigit(c: u8) bool {
     return (c >= '0' and c <= '9');
